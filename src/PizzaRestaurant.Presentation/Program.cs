09bf3c9d68f184d5c8a9d2963a1ca6d707a9bdab
Maks0s <mrmaksman01@mail.ru>
2024-03-20 11:15:01 +0300

    Add base Logging and Unexpected Errors Handling

--- src/PizzaRestaurant.Presentation/Program.cs
+++ src/PizzaRestaurant.Presentation/Program.cs
@@ -1,25 +1,57 @@
-var builder = WebApplication.CreateBuilder(args);
+using PizzaRestaurant.Presentation;
+using Serilog;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .CreateBootstrapLogger();
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    Log.Information("Started app building");
+
+    builder.Host.UseSerilog((_, loggerConfig) =>
+    {
+        var jsonConfig = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true, reloadOnChange: true)
+            .Build();
+
+        loggerConfig
+            .ReadFrom.Configuration(jsonConfig);
+    });
+
+    builder.Services.AddPresentation();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseExceptionHandler("/error");
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Error(ex, "Crushed during the building");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
