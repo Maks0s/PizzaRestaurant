b57307c08da937d1b45d8faa0c95a105ecb9773e
Maks0s <mrmaksman01@mail.ru>
2024-03-20 11:48:23 +0300

    Add BaseApiController for handling expected errors

---
+++ src/PizzaRestaurant.Presentation/Controllers/Common/BaseApiController.cs
@@ -0,0 +1,38 @@
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace PizzaRestaurant.Presentation.Controllers.Common
+{
+    [Controller]
+    public class BaseApiController : ControllerBase
+    {
+        protected ActionResult Problem(List<Error> errors)
+        {
+            if(errors.TrueForAll(error => error.Code.Equals("Validation failure")))
+            {
+                var msd = new ModelStateDictionary();
+
+                foreach(var error in errors)
+                {
+                    var invalidFieldAndDescription = error.Description.Split('-');
+                    msd.AddModelError(
+                            invalidFieldAndDescription[0],
+                            invalidFieldAndDescription[1]
+                        );
+                }
+
+                return ValidationProblem(msd);
+            }
+
+            var firstOccurredError = errors[0];
+
+            return Problem(
+                    statusCode: firstOccurredError.NumericType,
+                    title: firstOccurredError.Code,
+                    detail: firstOccurredError.Description,
+                    instance: Request.Path.Value
+                );
+        }
+    }
+}
