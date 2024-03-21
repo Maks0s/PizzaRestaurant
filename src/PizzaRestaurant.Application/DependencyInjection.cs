651ad4554d7966170ea4a38f7b46cd6aac6536d3
Maks0s <mrmaksman01@mail.ru>
2024-03-21 16:03:04 +0300

    Add Request Logging Behavior

---
+++ src/PizzaRestaurant.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PizzaRestaurant.Application.Common.Behaviors;
+using PizzaRestaurant.Application.Common.Interfaces;
+
+namespace PizzaRestaurant.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+                this IServiceCollection services
+            )
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+                cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
+
+            return services;
+        }
+    }
+}
