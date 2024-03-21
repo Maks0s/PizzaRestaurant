061b803948d308a6e733ef8cb570a356e58cb17f
Maks0s <mrmaksman01@mail.ru>
2024-03-21 16:00:34 +0300

    Add 'IPizzaRepository' and its implementation with all CRUD operations

---
+++ src/PizzaRestaurant.Application/Common/Interfaces/Persistence/IPizzaRepository.cs
@@ -0,0 +1,17 @@
+﻿using PizzaRestaurant.Domain.Entities;
+
+namespace PizzaRestaurant.Application.Common.Interfaces.Persistence
+{
+    public interface IPizzaRepository
+    {
+        public Task<Pizza?> AddPizzaAsync(Pizza pizzaToAdd);
+
+        public Task<Pizza?> GetPizzaAsync(Guid id);
+
+        public Task<ICollection<Pizza>> GetAllPizzasAsync();
+
+        public Task<int> UpdatePizzaAsync(Pizza updatedPizza);
+
+        public Task<int> DeletePizzaAsync(Guid id);
+    }
+}
---
+++ src/PizzaRestaurant.Infrastructure/Persistence/Repositories/PizzaRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaRestaurant.Application.Common.Interfaces.Persistence;
+using PizzaRestaurant.Domain.Entities;
+
+namespace PizzaRestaurant.Infrastructure.Persistence.Repositories
+{
+    public class PizzaRepository : IPizzaRepository
+    {
+        private readonly PizzaDbContext _context;
+
+        public PizzaRepository(PizzaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Pizza?> AddPizzaAsync(Pizza pizzaToAdd)
+        {
+            var addedPizza = 
+                await _context.Pizzas
+                    .AddAsync(pizzaToAdd);
+
+            await _context.SaveChangesAsync();
+
+            return addedPizza.Entity;
+        }
+
+        public async Task<Pizza?> GetPizzaAsync(Guid id)
+        {
+            var pizza = 
+                await _context.Pizzas
+                    .FindAsync(id);
+
+            return pizza;
+        }
+
+        public async Task<ICollection<Pizza>> GetAllPizzasAsync()
+        {
+            var allPizzas =
+                await _context.Pizzas
+                    .ToListAsync();
+
+            return allPizzas;
+        }
+
+        public async Task<int> UpdatePizzaAsync(Pizza updatedPizza)
+        {
+            int updatedPizzasCount =
+                await _context.Pizzas
+                    .Where(pizza => pizza.Id.Equals(updatedPizza.Id))
+                    .ExecuteUpdateAsync(pizza => pizza
+                        .SetProperty(p => p.Name, updatedPizza.Name)
+                        .SetProperty(p => p.CrustType, updatedPizza.CrustType)
+                        .SetProperty(p => p.Ingredients, updatedPizza.Ingredients)
+                        .SetProperty(p => p.Price, updatedPizza.Price)
+                    );
+
+            return updatedPizzasCount;
+        }
+
+        public async Task<int> DeletePizzaAsync(Guid id)
+        {
+            int deletedPizzasCount = 
+                await _context.Pizzas
+                    .Where(pizza => pizza.Id.Equals(id))
+                    .ExecuteDeleteAsync();
+
+            return deletedPizzasCount;
+        }
+    }
+}
