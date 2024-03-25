4f7cb76abd4ed2bc1863284eeb7905b64dccf746
Maks0s <mrmaksman01@mail.ru>
2024-03-25 10:16:00 +0300

    Add 'AddPizzaCommand'; Add related expected errors; Add 'AddPizzaEndpoint' in 'PizzaController'

---
+++ src/PizzaRestaurant.Application/Pizzas/Commands/AddPizzaCommand.cs
@@ -0,0 +1,13 @@
+﻿using PizzaRestaurant.Application.Common.Interfaces.CQRS;
+using PizzaRestaurant.Domain.Entities;
+
+namespace PizzaRestaurant.Application.Pizzas.Commands
+{
+    public record AddPizzaCommand(
+            string Name,
+            string CrustType,
+            string Ingredients,
+            decimal Price
+        )
+        : ICommand<Pizza?>;
+}
---
+++ src/PizzaRestaurant.Application/Pizzas/Commands/AddPizzaCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using ErrorOr;
+using PizzaRestaurant.Application.Common.AppErrors;
+using PizzaRestaurant.Application.Common.Interfaces.CQRS;
+using PizzaRestaurant.Application.Common.Interfaces.Persistence;
+using PizzaRestaurant.Domain.Entities;
+
+namespace PizzaRestaurant.Application.Pizzas.Commands
+{
+    public class AddPizzaCommandHandler
+        : ICommandHandler<AddPizzaCommand, Pizza?>
+    {
+        private readonly IPizzaRepository _pizzaRepository;
+
+        public AddPizzaCommandHandler(IPizzaRepository pizzaRepository)
+        {
+            _pizzaRepository = pizzaRepository;
+        }
+
+        public async Task<ErrorOr<Pizza?>> Handle(
+                AddPizzaCommand command,
+                CancellationToken cancellationToken
+            )
+        {
+            var pizzaToAdd = new Pizza()
+            {
+                Id = Guid.NewGuid(),
+                Name = command.Name,
+                CrustType = command.CrustType,
+                Ingredients = command.Ingredients,
+                Price = command.Price,
+            };
+
+            var addedPizza =
+                await _pizzaRepository.AddPizzaAsync(pizzaToAdd);
+
+            if (addedPizza is null)
+            {
+                return Errors.ServerDataManipulation.NotAdded();
+            }
+
+            return addedPizza;
+        }
+    }
+}
